4b29122670183a2cd3f813da7173966bf456b692
Frankccv <francescocaccavale95@gmail.com>
2023-05-12 12:21:21 +0200

    One json

--- kafka_script.py
+++ kafka_script.py
@@ -11,16 +11,15 @@ def read_json(namefile):
 	
 	data=json.load(f)
 	
-	for i in data['nodes']:
-		print(i)
-		  
+	print(data)
   	# Closing file
 	f.close()
 	return data
 
 # Check if any message exists in topic and update with new message
-def check_and_update_message(topic_name, consumer,producer,data):
-	
+def send_message(topic_name,producer,data):
+
+
 	msg_id = 1
 	key = str(msg_id).encode('utf-8')
 
@@ -55,21 +54,20 @@ def main():
 
 
 	if not topic_exists:
-	    topic_partitions = 1
-	    topic_replication_factor = 1
-	    new_topic = NewTopic(name=topic_name, num_partitions=topic_partitions, replication_factor=topic_replication_factor)
+
+	    new_topic = NewTopic(name=topic_name, num_partitions=1, replication_factor=1)
 	    admin_client.create_topics([new_topic])
 	    print('Created new topic:', topic_name)
 	else:
 	    print('Topic already exists:', topic_name)
-	consumer = KafkaConsumer(topic_name, bootstrap_servers=bootstrap_servers, auto_offset_reset='earliest', enable_auto_commit=True, group_id='my-group')
+
 
 	# Create a KafkaProducer instance
 	producer = KafkaProducer(bootstrap_servers=bootstrap_servers,value_serializer=lambda v: json.dumps(v).encode('utf-8'))
 
 	data= read_json(namefile)
 	   
-	check_and_update_message(topic_name,consumer,producer,data)
+	send_message(topic_name,consumer,producer,data)
 
 	
 	
